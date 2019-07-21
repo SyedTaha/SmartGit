e295aa448091105f405203f5cf58992b735dab15
Syed Taha <tahaanwer9@gmail.com>
2019-07-21 01:36:23 -0700

    Python File

---
+++ python.py
@@ -0,0 +1,93 @@
+# age = input("enter yourr age: ")
+# age = int(age)
+# if age >= 15 :
+#     print("congo")
+# else:
+#     print("sorry")
+
+# # winning_number = 5
+# import random
+# user_input = input("Guess a number b/w 1 to 10 : ")
+# ran_num = random.randint(1,10)
+# user_input= int(user_input)
+# if user_input == ran_num:
+#     print("you win")
+# else:
+#     print("try again")
+
+
+# age = input("enter your age: ")
+# age = int(age)
+# if age > 15:
+#     print("adult")
+# else:
+#     print("baby")
+
+
+
+
+
+
+# cow = input("how many cows do you have")
+# cow = int(cow)
+# if cow >= 50:
+#     print ("your entry is  possible")
+# else:
+#     print ("your entry is not possible")
+
+# num = int(input("enter number: "))
+
+# while num <= 10 :
+#     print("hi")
+#     num = num + 1 
+
+# name = input("name: ")
+# i = 1
+# while i <= 10:
+#     print(f"{name} {i}" )
+#     i = i + 1
+
+# total = 0
+# i = 1
+# while i <= 10:
+#     total = total + i
+#     print(total)
+#     i = i + 1
+
+
+
+# user_input = input("Guess a number b/w 1 to 10 : ")
+# # ran_num = random.randint(1,10)
+# # user_input= int(user_input)
+
+# age = int(input("Enter a number: "))
+# # high_number = int(input("Enter second number: "))
+# total = -0
+# if age <= 15:
+#     print("You are under age.")
+# elif age >=16:
+#     print("your a adult ")
+# else:
+#     print("try again")
+
+
+# def add_num(num_1 , num_2):
+#     return num_1 + num_2
+# print(add_num(6,6))
+
+
+# def user_name (first_name, last_name):
+#     return first_name + last_name
+# input_first_name = input("Enter first name: ")
+# input_last_name = input("Enter last name : ")
+# print(user_name(input_first_name, input_last_name))
+# print('First name lenght:',len(input_first_name),'\n','Last name lenght',len(input_last_name))
+
+
+def even_odd (num):
+    if num%2 == 0:
+        return 'even'
+    return 'odd'
+print(even_odd(10))
+
+
